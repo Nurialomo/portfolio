2e44a95b189282e62fac8fd246f6ebf272a5a0ce
Nuria <nurialomo@gmail.com>
2023-10-20 13:33:34 +0200

    Primera version

---
+++ js/funciones.js
@@ -0,0 +1,12 @@
+/*const nav=document.querySelector("nav");
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if(!entry.isIntersecting){
+    	nav.style.position=("sticky");
+    	console.log("dentro");
+    }
+  });
+});
+
+observer.observe(nav);*/
